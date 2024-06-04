e7bb782aa08585cc8c8eff5434bac8c0a0872ef2
KwwClw <neskung50@gmail.com>
2024-06-04 15:03:59 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
+const PORT = process.env.PORT || 8080
 
 http.createServer(function(req, res) {
     res.write("I'm alive!");
     res.end();
-}).listen(8080);
+}).listen(PORT);
