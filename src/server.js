0ba04111150a96d39958d2292b5d24c0de88bcf1
KwwClw <neskung50@gmail.com>
2024-08-09 14:06:11 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -23,7 +23,9 @@ client.once('ready', () => {
 
     const info = {
         start_time: formattedDatetime,
-        bot_name: client.user.username
+        bot_name: client.user.username,
+        ping: Date.now() - interaction.createdTimestamp,
+        api_ping: Math.round(client.ws.ping)
     }
 
     app.set('view engine', 'ejs');
@@ -36,42 +38,14 @@ client.once('ready', () => {
         })
     });
 
+    app.get('/qr', (req, res) => {
+        res.render('page/qr')
+    });
+
     app.listen(PORT, () => {
         console.log(`App listening on port ${PORT}`);
     });
 
 });
-
-// client.once('ready', () => {
-//     http.createServer(function (req, res) {
-//         if (req.url === '/') {
-//             fs.readFile(__dirname + '/src/views/page/index.ejs', function (err, data) {
-//                 if (err) {
-//                     res.writeHead(500);
-//                     res.end('Error loading index.ejs');
-//                 } else {
-//                     res.setHeader('Content-Type', 'text/ejs');
-//                     res.writeHead(200);
-//                     res.end(data);
-//                 }
-//             });
-//         } else if (req.url === '/status') {
-//             var datetime = new Date();
-//             var options = { timeZone: 'Asia/Bangkok' }; // Set the time zone to Thai time
-//             var formattedDatetime = datetime.toLocaleString('en-EN', options);
-//             res.setHeader('Content-Type', 'application/json');
-//             res.writeHead(200);
-//             res.end(JSON.stringify({
-//                 status: `${client.user.username} is online.`,
-//                 datetime: formattedDatetime
-//             }));
-//         } else {
-//             res.writeHead(404);
-//             res.end('Not Found');
-//         }
-//     }).listen(PORT, () => {
-//         console.log(`Server is online on port ${PORT}`);
-//     });
-// });
 
 client.login(process.env.TOKEN); // Replace with your bot token
